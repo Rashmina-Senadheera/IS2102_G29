c7c5302051571777efdbc1521e80fd2f0daafe10
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-11-15 01:11:00 +0530

    View all suppliers

---
+++ Event_Management/Event_Planner/Suppliers.php
@@ -0,0 +1,223 @@
+<?php
+include('eventplanner_sidenav.php');
+include('eventplanner_header.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../css/viewSuppliersEP.css">
+</head>
+
+<body>
+    <div class="grid-container-payments">
+        <div class="gridSearch">
+            <div class="searchSec">
+                <div class="page-title"> Suppliers </div>
+                <div class="input-container">
+                    <input class="input-field" type="text" placeholder="Search suppliers" name="search">
+                    <i class="fa fa-search icon"></i>
+                </div>
+                <button type="submit" class="srcButton">Search</button>
+            </div>
+        </div>
+        <div class="gridMain">
+            <div class="suppliers-cards-container">
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier01.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>Bravo Event Productions<br><span>
+                                    Founded in 1987. Bravo Event Productions is an award winning, full-service event planning and production company specializing in designing and staging world-class coporate, association, government, military and non-profit functions nationwide.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier02.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>Fruit & Chocolate sensations<br><span>
+                                    Our Company was the first in years to start Chocolate Fountains when nobody knew what it was? Year later we are skill here today for all types of events. We cater from 25 guests 800 guests we can make it happend.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier03.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>DECORENTO PARTY SUPPLIES<br><span>
+                                    Decorento Party Supplies is a leading party supplies company in Sri Lanka. We are the pioneers in the party supplies industry in Sri Lanka. We have been in the industry for over 20 years and have been providing our services to the public for over 10 years.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier01.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>Bravo Event Productions<br><span>
+                                    Founded in 1987. Bravo Event Productions is an award winning, full-service event planning and production company specializing in designing and staging world-class coporate, association, government, military and non-profit functions nationwide.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier02.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>Fruit & Chocolate sensations<br><span>
+                                    Our Company was the first in years to start Chocolate Fountains when nobody knew what it was? Year later we are skill here today for all types of events. We cater from 25 guests 800 guests we can make it happend.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier03.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>DECORENTO PARTY SUPPLIES<br><span>
+                                    Decorento Party Supplies is a leading party supplies company in Sri Lanka. We are the pioneers in the party supplies industry in Sri Lanka. We have been in the industry for over 20 years and have been providing our services to the public for over 10 years.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier01.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>Bravo Event Productions<br><span>
+                                    Founded in 1987. Bravo Event Productions is an award winning, full-service event planning and production company specializing in designing and staging world-class coporate, association, government, military and non-profit functions nationwide.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier02.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>Fruit & Chocolate sensations<br><span>
+                                    Our Company was the first in years to start Chocolate Fountains when nobody knew what it was? Year later we are skill here today for all types of events. We cater from 25 guests 800 guests we can make it happend.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Suppliers/supplier03.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>DECORENTO PARTY SUPPLIES<br><span>
+                                    Decorento Party Supplies is a leading party supplies company in Sri Lanka. We are the pioneers in the party supplies industry in Sri Lanka. We have been in the industry for over 20 years and have been providing our services to the public for over 10 years.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="" class="view-supplier">View</a>
+                        </li>
+                        <li>
+                            <a href="" class="request">Request a Quotation</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
