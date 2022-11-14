dd3d24a78523985fe94a6efd3d88a00846e7f68d
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-11-15 00:50:24 +0530

    view events created by the event planner

---
+++ Event_Management/Event_Planner/MyEvents.php
@@ -0,0 +1,217 @@
+<?php
+include('eventplanner_sidenav.php');
+include('eventplanner_header.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../css/eventPlannerMyevents.css">
+</head>
+
+<body>
+    <div class="grid-container-payments">
+        <div class="gridSearch">
+            <div class="searchSec">
+                <div class="page-title"> My Events </div>
+                <div class="input-container">
+                    <input class="input-field" type="text" placeholder="Search events" name="search">
+                    <i class="fa fa-search icon"></i>
+                </div>
+                <button type="submit" class="srcButton">Search</button>
+            </div>
+        </div>
+        <div class="gridMain">
+            <div class="my-events-container">
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event01.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>SEWWANDI'S BIRTHDAY<br><span>
+                                    The lovely Sewwandi was on cloud nine when she was pleasantly surprised by her loved ones and friends on her birthday!! The atmosphere and decorum created by DEVENT, truly set the tone to create a memorable experience for her. Do not hesitate to contact us in making your dream event a reality.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event02.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>the hottest wedding trend right now<br><span>
+                                    This is simply a wedding which takes place out in nature - preferably on a mountain top, or a slightly remote place surrounded by amazing scenery. The bride and groom often travels alone, or brings just the closest family.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event03.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>SWARA DAYADAYA<br><span>The first initiative undertaken by DEVENT, an event management firm aiming to reach the pinnacle of the industry. After thorough market research, we figured out one critical attention grasping issue faced by the wider community, which is the mental instability emerging due to the hectic monotonous routines of a substantial proportion of the population, ultimately leading to the deterioration of their overall wellbeing.</span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event01.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>SEWWANDI'S BIRTHDAY<br><span>
+                                    The lovely Sewwandi was on cloud nine when she was pleasantly surprised by her loved ones and friends on her birthday!! The atmosphere and decorum created by DEVENT, truly set the tone to create a memorable experience for her. Do not hesitate to contact us in making your dream event a reality.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event02.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>the hottest wedding trend right now<br><span>
+                                    This is simply a wedding which takes place out in nature - preferably on a mountain top, or a slightly remote place surrounded by amazing scenery. The bride and groom often travels alone, or brings just the closest family.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event03.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>SWARA DAYADAYA<br><span>The first initiative undertaken by DEVENT, an event management firm aiming to reach the pinnacle of the industry. After thorough market research, we figured out one critical attention grasping issue faced by the wider community, which is the mental instability emerging due to the hectic monotonous routines of a substantial proportion of the population, ultimately leading to the deterioration of their overall wellbeing.</span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event01.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>SEWWANDI'S BIRTHDAY<br><span>
+                                    The lovely Sewwandi was on cloud nine when she was pleasantly surprised by her loved ones and friends on her birthday!! The atmosphere and decorum created by DEVENT, truly set the tone to create a memorable experience for her. Do not hesitate to contact us in making your dream event a reality.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event02.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>the hottest wedding trend right now<br><span>
+                                    This is simply a wedding which takes place out in nature - preferably on a mountain top, or a slightly remote place surrounded by amazing scenery. The bride and groom often travels alone, or brings just the closest family.
+                                </span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card">
+                    <div class="content">
+                        <div class="imgBx">
+                            <img src="../images/Events/event03.jpg">
+                        </div>
+                        <div class="contentBx">
+                            <h3>SWARA DAYADAYA<br><span>The first initiative undertaken by DEVENT, an event management firm aiming to reach the pinnacle of the industry. After thorough market research, we figured out one critical attention grasping issue faced by the wider community, which is the mental instability emerging due to the hectic monotonous routines of a substantial proportion of the population, ultimately leading to the deterioration of their overall wellbeing.</span></h3>
+                        </div>
+                    </div>
+                    <ul class="sci">
+                        <li>
+                            <a href="">Edit</a>
+                        </li>
+                        <li>
+                            <a href="">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
