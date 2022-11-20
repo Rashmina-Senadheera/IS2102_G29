105ef92ee3c08870f3e72c44fc23ee0f60dbc0ef
Rashmina-Senadheera <84730333+Rashmina-Senadheera@users.noreply.github.com>
2022-11-20 13:36:27 +0530

    Delete Event_Management/Event_Management directory

--- Event_Management/Event_Management/activePage.js
+++
@@ -1,6 +0,0 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('.menu_items li a').forEach(link => {
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
-    }
-})
