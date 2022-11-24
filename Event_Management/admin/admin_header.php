44bc799c080a77fb18422e652440f3ae4e95648e
Rashmina-Senadheera <84730333+Rashmina-Senadheera@users.noreply.github.com>
2022-11-24 11:24:24 +0530

    Add files via upload

--- Event_Management/admin/admin_header.php
+++ Event_Management/admin/admin_header.php
@@ -19,8 +19,9 @@ if(!isset($_GET['reload'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="admin_header.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 
 </head>
 <body>
@@ -45,17 +46,7 @@ if(!isset($_GET['reload'])) {
                     
                 }
                 ?>
-                <div class="dropdown_content">
-                    <span class="dropdown_element">
-                        <img src="../images/profile.png" class="admin_profile">
-                        <a href="#">Profile</a>
-                    </span>
-                    <span class="dropdown_element" >
-                        <img src="../images/log-out.png" class="admin_logout">
-                        <a onclick="logout_check()" href="../logout.php">Logout</a>
-                        </span>
-
-                </div>
+                
                 </span>
 
                 <img src="../images/user.png" class="admin_user_logo">
