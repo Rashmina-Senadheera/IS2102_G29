ca2f97815c25148f25b944946ed460439cf07abe
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-12-11 16:02:10 +0530

    implement sign in 2

--- Event_Management/location_check.php
+++ Event_Management/location_check.php
@@ -1,17 +1,20 @@
-<?php 
-    include('constants.php');
-    $role = $_SESSION['role'];
+<?php
+include('constants.php');
+$role = $_SESSION['role'];
 ?>
 <html>
-    <body>
-    <script>              
-             var role = <?php echo json_encode($role); ?>;
-            if(role==='admin'){
-                location.replace('admin/dashboard.php');
-            }
-            else if(role === 'customer'){
-                location.replace('landing_page.php');
-                }
-            </script>
-    </body>
+
+<body>
+    <script>
+        var role = <?php echo json_encode($role); ?>;
+        if (role === 'admin') {
+            location.replace('admin/dashboard.php');
+        } else if (role === 'customer') {
+            location.replace('landing_page.php');
+        } else if (role === 'event_planner') {
+            location.replace('Event_Planner/Suppliers.php');
+        }
+    </script>
+</body>
+
 </html>
--- Event_Management/sign_in2.php
+++ Event_Management/sign_in2.php
@@ -1,6 +1,59 @@
 <?php
 include('constants.php');
 include('Event_Planner/eventplanner_header.php');
+include('Event_Planner/controllers/commonFunctions.php');
+
+// define variables and set to empty values
+$email = $password = "";
+
+// check user already logged in
+if (isset($_SESSION['role']) && isset($_SESSION['user_id']) && isset($_SESSION['user-name'])) {
+    header("location: location_check.php");
+} else {
+    unset($_SESSION['role']);
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user-name']);
+}
+
+// check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = chekcInput($_POST["email"]);
+    $password = chekcInput($_POST["password"]);
+
+    // check if email and password empty
+    if (empty($email)) {
+        $_SESSION['error-email'] = "Email is required";
+    }
+    if (empty($password)) {
+        $_SESSION['error-password'] = "Password is required";
+    }
+
+    // check email in the database
+    $sql = "SELECT * FROM user WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $db_password = $row['password'];
+
+        // check password
+        if (password_verify($password, $db_password)) {
+            // set session variables
+            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['role'] = $row['role'];
+            $_SESSION['user-name'] = $row['name'];
+
+            // redirect to the relevant page
+            header("location: location_check.php");
+        } else {
+            $_SESSION['error'] = "Incorrect password";
+            $_SESSION['email'] = $email;
+        }
+    } else {
+        $_SESSION['error'] = "Email not found!";
+        $_SESSION['email'] = $email;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -22,14 +75,23 @@ include('Event_Planner/eventplanner_header.php');
             </div>
             <div class="white-box">
                 <p class="heading">Sign In</p>
-                <form class="login-form">
+                <!-- Show errors -->
+                <div class="signinTopError"><?php echo showSessionMessage('error') ?></div>
+                <form class="login-form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                     <div class="input">
-                        <input type="text" class="input-field" required />
+                        <input type="text" value="<?php
+                                                    if (isset($_SESSION['email'])) {
+                                                        echo $_SESSION['email'];
+                                                        unset($_SESSION['email']);
+                                                    }
+                                                    ?>" name="email" class="input-field" required />
                         <label class="input-label">Email</label>
+                        <div class="signinError"><?php echo showSessionMessage('error-email') ?></div>
                     </div>
                     <div class="input">
-                        <input type="password" class="input-field" required />
+                        <input type="password" name="password" class="input-field" required />
                         <label class="input-label">Password</label>
+                        <div class="signinError"><?php echo showSessionMessage('error-password') ?></div>
                     </div>
                     <div class="action">
                         <button class="action-button">Sign In</button>
