56f512270e7385b4e16f0c1ec178f116728ecab0
Rashmina-Senadheera <84730333+Rashmina-Senadheera@users.noreply.github.com>
2022-12-12 08:09:25 +0530

    Delete Event_Management/admin directory

--- Event_Management/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    //inlcude constants
-    include('../config/constants.php');
-    //destroy the session and redirect to  login page
-    session_destroy();//unsets $_SESSION['user']
-    
-    header('location:'.SITEURL.'admin/login.php');
-?>
