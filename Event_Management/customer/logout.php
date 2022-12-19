5ad3b08292dd276b7105f80dc7d19ee731c664d9
Daweendri <83944867+Daweendri@users.noreply.github.com>
2022-12-19 19:10:20 +0530

    Delete Event_Management/customer directory

--- Event_Management/customer/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // include('constants.php');
-    // session_destroy();
-    // header('http://localhost/Event_Management/index.php');
-
-    session_destroy(); 
-    header("location: ../index.php"); 
-?>
