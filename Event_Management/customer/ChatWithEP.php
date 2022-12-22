4f1a42fbc72fca595b4e2cf00d7d8bee0571d010
Daweendri <83944867+Daweendri@users.noreply.github.com>
2022-12-22 11:25:47 +0530

    Update ChatWithEP.php

--- Event_Management/customer/ChatWithEP.php
+++ Event_Management/customer/ChatWithEP.php
@@ -17,7 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="chat.css">
+    <link rel="stylesheet" href="ChatWithEP.css">
     <script src="https://kit.fontawesome.com/bf10032598.js" crossorigin="anonymous"></script>
 </head>
 <body>
