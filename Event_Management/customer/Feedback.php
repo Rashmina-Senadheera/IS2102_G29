93528a91dc2cf3c3a23fe66dc294867a26648396
Daweendri <83944867+Daweendri@users.noreply.github.com>
2022-12-23 10:56:29 +0530

    update feedback

--- Event_Management/customer/Feedback.php
+++ Event_Management/customer/Feedback.php
@@ -53,11 +53,11 @@ include('customer_header.php');
                         <div >
                             </div>
                             <center>Date:&nbsp;&nbsp;<center>
-                            <div ><center><input type="date" class="form-control" placeholder=""  name="date"  min="2022-12-23" max="2025-12-31" style="width:300px;" value="<?php echo $date; ?>" required></div>
+                            <div ><center><input type="date" class="form-control" placeholder=""  name="date"  min="2022-12-23" max="2025-12-31" style="width:300px;" value="<?php echo $date; ?>" readonly></div>
                         </div><br>
                         <div>
                         <center>Event Planner's Name:&nbsp;&nbsp;<center>
-                            <div ><center><input type="name" class="form-control" placeholder="Event Planner's Name" name="name" style="width:300px;" min="5" max="25" value="<?php echo (isset($_GET['name'])) ? $_GET['name'] : "" ?>" required></div>
+                            <div ><center><input type="name" class="form-control" placeholder="Event Planner's Name" name="name" style="width:300px;" min="5" max="25" value="<?php echo (isset($_GET['name'])) ? $_GET['name'] : "" ?>" readonly></div>
                         </div>
                     </div>
                 <div >
