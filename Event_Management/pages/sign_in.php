580feba8262c35fce7b416c15e9dbeb6acc7c114
Shamin Fernando <2020is037@stu.ucsc.cmb.ac.lk>
2023-01-09 12:12:48 +0530

    changes to sign in

--- Event_Management/pages/sign_in.php
+++ Event_Management/pages/sign_in.php
@@ -1,7 +1,7 @@
 <?php
 include('constants.php');
 include('header-sign.php');
-include('pages/Event_Planner/controllers/commonFunctions.php');
+include('controllers/commonFunctions.php');
 
 // define variables and set to empty values
 $email = $password = "";
@@ -72,8 +72,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/sign_in2.css">
-    <link rel="stylesheet" href="./css/navigationBar.css">
+    <link rel="stylesheet" href="../css/sign_in2.css">
+    <link rel="stylesheet" href="../css/navigationBar.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 </head>
 
@@ -87,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="loginbody">
         <div class="box">
             <div class="image-box">
-                <img src="images/Login-2.svg" class="sign_in_logo" />
+                <img src="../images/Login-2.svg" class="sign_in_logo" />
             </div>
             <div class="white-box">
                 <p class="heading">Sign In</p>
