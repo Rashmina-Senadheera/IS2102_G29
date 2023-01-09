3cf5a526d7211b93cd57dd16290a212331a82a81
Rashmina-Senadheera <84730333+Rashmina-Senadheera@users.noreply.github.com>
2023-01-09 12:00:18 +0530

    css update 1

--- Event_Management/pages/admin/admin_nav.php
+++ Event_Management/pages/admin/admin_nav.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/admin_nav.css">
+    <link rel="stylesheet" href="../../css/admin_nav.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <script defer src="javascript/activeNav.js"></script>    
