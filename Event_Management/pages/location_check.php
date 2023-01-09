5c078bca1084ddd6bf3d5bf69c7952a3625918f3
Shamin Fernando <2020is037@stu.ucsc.cmb.ac.lk>
2023-01-09 12:14:41 +0530

    file path location check

--- Event_Management/pages/location_check.php
+++ Event_Management/pages/location_check.php
@@ -15,11 +15,11 @@ if (isset($_SESSION['role'])) {
     <script>
         var role = <?php echo json_encode($role); ?>;
         if (role === 'admin') {
-            location.replace('pages/admin/dashboard.php');
+            location.replace('admin/dashboard.php');
         } else if (role === 'customer') {
-            location.replace('pages/customer/customer_quotation.php');
+            location.replace('customer/customer_quotation.php');
         } else if (role === 'event_planner') {
-            location.replace('pages/Event_Planner/Requests.php');
+            location.replace('Event_Planner/Requests.php');
         } else if (role === 'supplier') {
             location.replace('Supplier/dashboard.php');
         }
