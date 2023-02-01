e452f0c2a9d499df6d06ef65e91c59cc554fc330
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-02-01 14:30:34 +0530

    Update Payments.php

--- Event_Management/pages/customer/Payments.php
+++ Event_Management/pages/customer/Payments.php
@@ -12,8 +12,8 @@ include('customer_header.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/eventPlannerMain.css">
-    <link rel="stylesheet" href="../css/Custcss2.css">
+    <link rel="stylesheet" href="../../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../../css/Custcss2.css">
 <style>
     .cards img{
         width: 100px;
