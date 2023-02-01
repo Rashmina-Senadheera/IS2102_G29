35567b2cdd6622bba3e0356ee1db8704ab1074fc
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-02-01 14:28:38 +0530

    Update Messages.php

--- Event_Management/pages/customer/Messages.php
+++ Event_Management/pages/customer/Messages.php
@@ -17,11 +17,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/viewSuppliersEP.css">
-    <link rel="stylesheet" href="../css/filterEP.css">
-    <link rel="stylesheet" href="../css/eventPlannerMain.css">
-        <link rel="stylesheet" href="../css/eventPlannerMain.css">
-    <link rel="stylesheet" href="../css/chat.css">
+    <link rel="stylesheet" href="../../css/viewSuppliersEP.css">
+    <link rel="stylesheet" href="../../css/filterEP.css">
+    <link rel="stylesheet" href="../../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../../css/chat.css">
     <script src="https://kit.fontawesome.com/bf10032598.js" crossorigin="anonymous"></script>
 </head>
 
