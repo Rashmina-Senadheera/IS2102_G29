9a7496b0e32d671a320a76740703971cd3cec31d
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-02-01 14:21:54 +0530

    Update Event2.php

--- Event_Management/pages/customer/Event2.php
+++ Event_Management/pages/customer/Event2.php
@@ -19,8 +19,8 @@ $sql = "SELECT * FROM events_c WHERE id=?";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/eventPlannerMain.css">
-    <link rel="stylesheet" href="../css/Custcss2.css">
+    <link rel="stylesheet" href="../../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../../css/Custcss2.css">
 <style>
 
 </style>
