a1a6fb4883dab2869457427d761716ef9796196e
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-02-01 14:33:05 +0530

    Update QuotationNw.php

--- Event_Management/pages/customer/QuotationNw.php
+++ Event_Management/pages/customer/QuotationNw.php
@@ -11,8 +11,8 @@ include('customer_header.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/eventPlannerMain.css">
-    <link rel="stylesheet" href="../css/Custcss2.css">
+    <link rel="stylesheet" href="../../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../../css/Custcss2.css">
 </head>
 
 <body>
