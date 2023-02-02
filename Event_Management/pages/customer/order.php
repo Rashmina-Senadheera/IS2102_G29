2654657d23d3f82d101518de0322906d1d7e63d3
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-02-02 14:12:36 +0530

    Update order.php

--- Event_Management/pages/customer/order.php
+++ Event_Management/pages/customer/order.php
@@ -12,8 +12,8 @@ include('customer_header.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/eventPlannerMain.css">
-    <link rel="stylesheet" href="../css/Custcss3.css">
+    <link rel="stylesheet" href="../../css/eventPlannerMain.css">
+    <link rel="stylesheet" href="../../css/Custcss3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
