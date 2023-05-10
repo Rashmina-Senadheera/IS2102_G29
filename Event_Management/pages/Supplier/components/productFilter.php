bc827745a311c264113484825759af462edb3b07
Shamin Fernando <hhshaminf@gmail.com>
2023-05-10 12:09:37 +0530

    commit to supplier

---
+++ Event_Management/pages/Supplier/components/productFilter.php
@@ -0,0 +1,38 @@
+<div class="filter">
+    <div class="search">
+        <div class = 'input-container'>
+            <input class = 'input-field-filter' type = 'text' placeholder = 'Search payments' name = 'search'>
+            <i class = 'fa fa-search icon'></i>
+        </div>
+    </div>
+    <div class="category">
+        <div class="filter-heading">Filter by Category</div>
+        <div class="category-list">
+            <ul>
+                <li><input type="checkbox">All</li>
+                <li><input type="checkbox">Venue</li>
+                <li><input type="checkbox">Entertainment</li>
+                <li><input type="checkbox">Catering</li>
+                <li><input type="checkbox">Photography</li>
+                <li><input type="checkbox">Transport</li>
+                <li><input type="checkbox">Beverages</li>
+                <li><input type="checkbox">Florists</li>
+                <li><input type="checkbox">Decoration</li>
+                <li><input type="checkbox">Lighting</li>
+                <li><input type="checkbox">Audio/Vedio</li>
+            </ul>
+        </div>
+        <div class="sort">
+        <div class="filter-heading">Filter by Date</div>
+        <div class="sort-list">
+            <ul>
+                <li>
+                    <select name="date" id="date-sort">
+                        <option value="oldest">Oldest on Top</option>
+                        <option value="newest">Newest on Top</option>
+                    </select>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
