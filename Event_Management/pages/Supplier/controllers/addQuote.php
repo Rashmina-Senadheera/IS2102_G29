baf15264d6c774f7834d86b87a70384add52887b
Shamin Fernando <hhshaminf@gmail.com>
2023-05-12 13:06:29 +0530

    supplier send quoatation

--- Event_Management/pages/Supplier/controllers/addQuote.php
+++ Event_Management/pages/Supplier/controllers/addQuote.php
@@ -21,41 +21,66 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
     $ep_id = checkInput($_POST['ep_id']);
     $req_id = checkInput($_POST['req_id']);
     $for_cus_req = checkInput($_POST['for_cus_req']);
+    $terms= isset($_POST['terms']);
+    $remarks_quote= checkInput($_POST['description']);
 
-    $sql = "INSERT INTO  supplier_quotation(date, title, description, cost, type, status, supplier_id, ep_id, req_id, for_cus_req) VALUES(?,?,?,?,?,?,?,?,?,?)";
+    $sql = "INSERT INTO  supplier_quotation(date, title, description, cost, type, status, supplier_id, ep_id, req_id, for_cus_req,remarks_quote) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
 
-    if ($stmt = $conn->prepare($sql)) {
+  
+    if (empty($cost)) {
+        $_SESSION['error-cost'] = "Enter Cost";
+    }
+    if (empty($terms)) {
+        $_SESSION['error-terms'] = "Must Agree to terms and Conditions";
+    }
+    if (empty($remarks_quote)) {
+        $_SESSION['error-descript'] = "Enter Remarks";
+    }
+    
 
-        // Bind variables to the prepared statement as parameters
-        $stmt->bind_param('sssdssiiii', $param_date, $param_title, $param_description, $param_cost, $param_type, $param_status, $param_supplier_id, $param_ep_id, $param_req_id, $param_for_cus_req);
-        // Set parameters
-        $param_date = $date;
-        $param_title = $title;
-        $param_description = $p_description;
-        $param_cost = $cost;
-        $param_type = $ptype;
-        $param_status = $status;
-        $param_supplier_id = $supplier_id;
-        $param_ep_id = $ep_id;
-        $param_req_id = $req_id;
-        $param_for_cus_req = $for_cus_req;
+    // Validate package name
+    // If there are any errors go back
+    if (
+        isset($_SESSION['error-cost']) || isset($_SESSION['error-terms']) || isset($_SESSION['error-descript'])
+    ) {
+        echo "<script> history.back(); </script>";
+    } else {
+        if ($stmt = $conn->prepare($sql)) {
 
-        // Attempt to execute the prepared statement
-        if ($stmt->execute()) {
-            $_SESSION['success'] = "Quotation sent successfully".$req_id;
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param('sssdssiiiis', $param_date, $param_title, $param_description, $param_cost, $param_type, $param_status, $param_supplier_id, $param_ep_id, $param_req_id, $param_for_cus_req,$param_remarks);
+            // Set parameters
+            $param_date = $date;
+            $param_title = $title;
+            $param_description = $p_description;
+            $param_cost = $cost;
+            $param_type = $ptype;
+            $param_status = $status;
+            $param_supplier_id = $supplier_id;
+            $param_ep_id = $ep_id;
+            $param_req_id = $req_id;
+            $param_for_cus_req = $for_cus_req;
+             $param_remarks = $remarks_quote;
 
-            // Update the status of the request
-            $sql = "UPDATE request_supplier_quotation SET status = 'Completed' WHERE request_id = $req_id";
-            $conn->query($sql);
+            
 
-            header("location: ../rs-list.php");
-        } else {
-            $_SESSION['error'] = "Error: " . $conn->error;
-            echo "<script> window.history.go(-1); </script>";
-            exit();
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                $_SESSION['success'] = "Quotation sent successfully".$req_id;
 
+                // Update the status of the request
+                $sql = "UPDATE request_supplier_quotation SET status = 'Completed' WHERE request_id = $req_id";
+                $conn->query($sql);
+
+                header("location: ../rs-list.php");
+            } else {
+                $_SESSION['error'] = "Error: " . $conn->error;
+                echo "<script> window.history.go(-1); </script>";
+                exit();
+
+            }
+            $conn->close();
         }
-        $conn->close();
     }
 }
 ?>
--- Event_Management/pages/Supplier/order-view.php
+++ Event_Management/pages/Supplier/order-view.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/eventPlannerMain.css">
-    <link rel="stylesheet" href="../../css/profileEP.css">
+    <link rel="stylesheet" href="../../css/profileSup.css">
     <link rel="stylesheet" href="../css/quote-view.css">
 </head>
 
